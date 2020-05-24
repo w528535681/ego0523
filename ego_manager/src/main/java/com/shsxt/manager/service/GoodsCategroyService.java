930de8c2bf951bdd0e09be6c81ecce17ad0ab1cb
wy <wy528535681@163.com>
2020-05-24 21:05:19 +0800

    商品分类列表显示，前后台实现

---
+++ ego_manager/src/main/java/com/shsxt/manager/service/GoodsCategroyService.java
@@ -0,0 +1,40 @@
+package com.shsxt.manager.service;
+
+import com.shsxt.common.result.BaseResult;
+import com.shsxt.manager.pojo.GoodsCategory;
+import com.shsxt.manager.vo.GoodsCategoryVo;
+
+import java.util.List;
+
+/**
+ * 商品分类service
+ */
+public interface GoodsCategroyService {
+
+	/**
+	 * 查询所有顶级分类
+	 * @return
+	 */
+	List<GoodsCategory> selectTopList();
+
+	/**
+	 * 通过父id查询所有分类
+	 * @param parentId
+	 * @return
+	 */
+	List<GoodsCategory> selectListByParentId(Short parentId);
+
+	/**
+	 * 保存
+	 * @param goodsCategory
+	 * @return
+	 */
+	BaseResult save(GoodsCategory goodsCategory);
+
+	/**
+	 * 查询所有分类List
+	 * @return
+	 */
+	public List<GoodsCategoryVo> selectAllList();
+
+}
